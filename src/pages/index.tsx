82f9aca175ff901f010309815c19f60f7fab0e04
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-04-24 01:43:32 -0300

    Adding axios and SSG in project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,37 @@
-import { Header } from "../components/Header";
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
 
-export default function Home() {
+type Episodes = {
+  id: string,     
+  title: string,
+  members: string,
+  published_at: string,
+}
+type propsGetStaticProps = {
+  episodes: Episodes[]
+}
+
+export default function Home(props:propsGetStaticProps) {
   return (
     <>
+      <p>{JSON.stringify(props.episodes)}</p>
     </>
   )
+}
+
+export const getStaticProps:GetStaticProps= async() =>{
+  const {data} = await api.get('/episodes', { 
+    params: { 
+      _limit: 12, 
+      _sort: 'published_at',
+      _order: 'desc',
+    }
+   });
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60*60*8,
+  }
 }
