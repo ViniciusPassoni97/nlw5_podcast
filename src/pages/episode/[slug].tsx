1c97e0b207e16713ee2e3fd6873ae80d05a1f6c4
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-04-24 22:53:02 -0300

    Adding episode page

--- src/pages/episode/[slug].tsx
+++ src/pages/episode/[slug].tsx
@@ -1,25 +1,76 @@
 import { format, parseISO } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
-import { GetStaticProps } from 'next';
-import { useRouter } from 'next/router';
+import { GetStaticProps, GetStaticPaths } from 'next';
+import Image from 'next/image';
 import { api } from '../../services/api';
 import { convertDurarionToTime } from '../../utils/convertDurarionToTime';
+import styles from './episode.module.scss';
+import Link from 'next/link';
 
-export default function Episodes () {
-    const router = useRouter();
+type Episodes = {
+    id: string;
+    title: string;
+    thumbnail: string;
+    description: string
+    members: string;
+    duration: number;
+    durationAsString: string;
+    url: string;
+    publishedAt: string;
+}
+type propsGetStaticProps = {
+    episode: Episodes,
+}
 
-    return(
-        <h1>{router.query.slug}</h1>
+export default function Episodes({ episode }: propsGetStaticProps) {
+
+    return (
+        <div className={styles.containerEpisodes}>
+            <div className={styles.thumbnailContainer}>
+                <Link href="/">
+                    <button type="button">
+                        <img src="/arrow-left.svg" alt="Voltar" />
+                    </button>
+                </Link>
+                <Image
+                    width={700}
+                    height={160}
+                    src={episode.thumbnail}
+                    objectFit="cover"
+                />
+                <button type="button">
+                    <img src="/play.svg" alt="Tocar episódio" />
+                </button>
+            </div>
+            <header>
+                <h1>{episode.title}</h1>
+                <span>{episode.members}</span>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.durationAsString}</span>
+            </header>
+
+            <div
+                className={styles.description}
+                dangerouslySetInnerHTML={{ __html: episode.description }}
+            />
+        </div>
     )
 }
 
 
-export const getStaticProps: GetStaticProps = async (ctx) => {
-    const {slug} = ctx.params;
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [],
+        fallback: 'blocking'
+    }
+}
 
-    const {data} = await api.get(`episode/${slug}`)
-    
-    const episode ={
+export const getStaticProps: GetStaticProps = async (ctx) => {
+    const { slug } = ctx.params;
+
+    const { data } = await api.get(`/episodes/${slug}`);
+
+    const episode = {
         id: data.id,
         title: data.title,
         thumbnail: data.thumbnail,
@@ -29,10 +80,10 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
         durationAsString: convertDurarionToTime(data.file.duration),
         description: data.description,
         url: data.file.url,
-      }
+    }
 
     return {
-        props: {},
-        revalidate: 60*60*24,
+        props: { episode },
+        revalidate: 60 * 60 * 24,
     }
 }
